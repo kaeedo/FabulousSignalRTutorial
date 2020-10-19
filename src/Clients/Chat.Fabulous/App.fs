9e428b132e0a9ab2303272d07204b373a240bc08
Ito, Kai <kai.ito@zuehlke.com>
2020-10-19 14:51:53 +0200

    Add new droid project

---
+++ src/Clients/Chat.Fabulous/App.fs
@@ -0,0 +1,94 @@
+﻿namespace Chat.Fabulous
+
+open System
+open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+open Microsoft.AspNetCore.SignalR.Client
+
+module App = 
+    type Model = 
+      { Messages: string list
+        EntryText: string
+        ChatHub: HubConnection }
+
+    type Msg = 
+        | EntryText of string
+        | SendMessage
+        | ReceivedMessage of string
+        | NoOp
+
+    // https://montemagno.com/real-time-communication-for-mobile-with-signalr/
+    // https://nicksnettravels.builttoroam.com/android-certificates/
+    let initModel = 
+        let hub = 
+            HubConnectionBuilder()
+                .WithUrl("http://10.193.16.165:5000/chathub")
+                .WithAutomaticReconnect()
+                .Build()
+
+        hub.StartAsync() |> Async.AwaitTask |> ignore
+
+        { Messages = []; EntryText = String.Empty; ChatHub = hub }
+
+    let setupListeners (dispatch: Msg -> unit) (hub: HubConnection) =
+        hub.On<string>("ReceiveMessage", fun message ->
+            dispatch <| ReceivedMessage message
+        ) |> ignore
+
+    let init () = 
+        initModel, Cmd.ofSub (fun (dispatch: Msg -> unit) -> setupListeners dispatch initModel.ChatHub)
+
+    let sendMessageCmd (model: Model) =
+        async {
+            do! model.ChatHub.InvokeAsync("SendMessageToAll", model.EntryText) |> Async.AwaitTask
+            return NoOp
+        }
+        |> Cmd.ofAsyncMsg
+
+    let update msg model =
+        match msg with
+        | SendMessage -> { model with EntryText = String.Empty }, (sendMessageCmd model)
+        | ReceivedMessage m -> { model with Messages = m :: model.Messages }, Cmd.none
+        | EntryText t -> { model with EntryText = t }, Cmd.none
+        | NoOp -> model, Cmd.none
+
+    let chatMessages (model: Model) =
+        let messages =
+            model.Messages
+            |> List.map (fun m -> View.Label(text = m))
+        View.ScrollView(View.StackLayout(children = messages))
+
+    let view (model: Model) dispatch =
+        View.ContentPage(
+          content = View.Grid(
+            columnSpacing = 2.0,
+            rowSpacing = 2.0,
+            rowdefs = [Stars 10.0; Stars 1.0],
+            coldefs = [Star; Star; Star],
+            padding = Thickness 5.0,
+            children = [ 
+                (chatMessages model).ColumnSpan(3)
+                View.Entry(
+                    placeholder = "Enter chat message",
+                    text = model.EntryText,
+                    textChanged = (fun args -> dispatch (EntryText args.NewTextValue))
+                ).Row(1).ColumnSpan(2)
+                View.Button(
+                    text = "Send", 
+                    command = (fun () -> dispatch SendMessage)
+                ).Row(1).Column(2)
+            ]))
+
+    // Note, this declaration is needed if you enable LiveUpdate
+    let program = XamarinFormsProgram.mkProgram init update view
+
+type App () as app = 
+    inherit Application ()
+
+    let runner = 
+        App.program
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> XamarinFormsProgram.run app
--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -3,35 +3,33 @@
 open System
 
 open Giraffe
-open Fable.Remoting.Server
-open Fable.Remoting.Giraffe
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.SignalR
 
-type ShoppingListbHub() =
+type ChatHub() =
     inherit Hub()
-    member __.UpsertItem(item: string, isDone: bool) =
-        printfn "Received: %s, Is done: %b" item isDone
-        __.Clients.All.SendAsync("UpsertItem", item + item, isDone)
+    member __.SendMessageToAll(message: string) =
+        printfn "Received: %s" message
+        __.Clients.All.SendAsync("ReceiveMessage", message)
 
 module Server =
     let webApp : HttpHandler =
-        Remoting.createApi()
-        |> Remoting.fromValue RemoteApi.shoppingApi
-        |> Remoting.withRouteBuilder Shared.Route.builder
-        |> Remoting.buildHttpHandler
+        choose [
+            GET >=> route "/" >=> text "hello"
+        ]
 
     let configureApp (app : IApplicationBuilder) =
+        // Needed together with endpoints
         app.UseRouting() |> ignore
         app.UseEndpoints(fun endpoints ->
-            endpoints.MapHub<ShoppingListbHub>("/shoppinglisthub") |> ignore
+            endpoints.MapHub<ChatHub>("/chathub") |> ignore
         ) |> ignore
+
         app.UseGiraffe webApp
 
     let configureServices (services : IServiceCollection) =
-        // Add Giraffe dependencies
         services.AddGiraffe() |> ignore
         services.AddSignalR() |> ignore
 
@@ -45,3 +43,30 @@ module Server =
             .Build()
             .Run()
         0
+
+
+        // xml/network_security_config.xml
+//<?xml version="1.0" encoding="utf-8"?>
+//<network-security-config>
+//    <base-config cleartextTrafficPermitted="true" />
+//</network-security-config>
+
+
+// properties/androimanifest
+//<?xml version="1.0" encoding="utf-8"?>
+//<manifest xmlns:android="http://schemas.android.com/apk/res/android" 
+//          android:versionCode="1" 
+//          android:versionName="1.0" 
+//          package="dev.hashset.chat.fabulous.droid">
+//  <uses-sdk android:minSdkVersion="26" android:targetSdkVersion="28" />
+//  <application 
+//      android:allowBackup="true" 
+//      android:icon="@mipmap/ic_launcher" 
+//      android:label="@string/app_name" 
+//      android:roundIcon="@mipmap/ic_launcher_round" 
+//      android:supportsRtl="true" 
+//      android:theme="@style/AppTheme"
+//      android:networkSecurityConfig="@xml/network_security_config">
+//  </application>
+//  <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+//</manifest>
